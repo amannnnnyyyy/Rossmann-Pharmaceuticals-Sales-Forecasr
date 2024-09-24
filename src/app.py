041e5fcad631d7b780ccf5c675c6c0def1debeeb
Amanuel Moha <amanuelmoha.official@gmail.com>
2024-09-24 16:48:31 +0300

    task 3

--- src/app.py
+++ src/app.py
@@ -5,8 +5,16 @@ import numpy as np
 import os
 import sys
 sys.path.append('../')
+from fastapi.staticfiles import StaticFiles
+from sklearn.preprocessing import StandardScaler
 
-# Load the trained model
+
+
+def load_scaler(scaler_path):
+    with open(scaler_path, 'rb') as file:
+        scaler = pickle.load(file)
+    return scaler
+
 def load_model(model_path):
     with open(model_path, 'rb') as file:
         model = pickle.load(file)
@@ -20,12 +28,20 @@ def get_latest_model(directory):
     latest_model = max(model_files, key=lambda f: os.path.getmtime(os.path.join(directory, f)))
     return os.path.join(directory, latest_model)
 
-# Change this to your actual model path
+
+
 model_directory = "../notebooks/models/"
 model_path = get_latest_model(model_directory)
 model = load_model(model_path)
 
+scaler_path = "../notebooks/scaler.pkl"
+scaler = load_scaler(scaler_path)
+
+
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="../static"), name="static")
+
 
 # Define input data model
 class PredictionInput(BaseModel):
@@ -65,10 +81,18 @@ class PredictionInput(BaseModel):
 
 @app.post("/predict")
 def predict(input_data: PredictionInput):
-    # Prepare the input data for the model,
-    data = np.array([[input_data.Id, input_data.Store,input_data.DayOfWeek,input_data.Open,input_data.Promo,input_data.StateHoliday,input_data.SchoolHoliday,input_data.StoreType,input_data.Assortment,input_data.CompetitionDistance,input_data.CompetitionOpenSinceMonth,input_data.CompetitionOpenSinceYear,input_data.Promo2,input_data.Promo2SinceWeek,input_data.Promo2SinceYear,input_data.PromoInterval,input_data.Year	,input_data.Month	,input_data.Day	,input_data.WeekOfYear	,input_data.CompetitionOpen	,input_data.Promo2Open	,input_data.IsPromo2Month	,input_data.day_of_week	,input_data.is_weekend	,input_data.days_to_holiday	,input_data.days_after_holiday	,input_data.beginning_of_month	,input_data.mid_of_month	,input_data.end_of_month	,input_data.is_month_end	,input_data.is_month_start	,input_data.quarter]]) 
-    # Make prediction
-    prediction = model.predict(data)  # Use scaled data if necessary
+    data = np.array([[input_data.Store,input_data.DayOfWeek,input_data.Open,input_data.Promo,
+                      input_data.StateHoliday,input_data.SchoolHoliday,input_data.StoreType,
+                      input_data.Assortment,input_data.CompetitionDistance,
+                      input_data.CompetitionOpenSinceMonth,input_data.CompetitionOpenSinceYear,
+                      input_data.Promo2,input_data.Promo2SinceWeek,input_data.Promo2SinceYear,
+                      input_data.PromoInterval,input_data.Year	,input_data.Month	,input_data.Day	,
+                      input_data.WeekOfYear	,input_data.CompetitionOpen	,input_data.Promo2Open	,
+                      input_data.IsPromo2Month	,input_data.day_of_week	,input_data.is_weekend	,
+                      input_data.days_to_holiday	,input_data.days_after_holiday	,
+                      input_data.beginning_of_month	,input_data.mid_of_month	,input_data.end_of_month	,
+                      input_data.is_month_end	,input_data.is_month_start	,input_data.quarter]]) 
+    scaled_data = scaler.transform(data)
+    scaled_prediction = model.predict(scaled_data)
 
-    # Format the response
-    return {"prediction": prediction[0]}  # Return the prediction as a JSON response
+    return {"prediction": scaled_prediction[0]}
